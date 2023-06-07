626a273d0cc7f6a33ef5407388bcd54c17a55b06
Damián Pumar <damianpumar@gmail.com>
2023-06-07 21:57:50 +0200

    ✨ Improve tests

---
+++ apps/starter/src/application/test.test.ts
@@ -0,0 +1,37 @@
+import { IEventDispatcher } from "@codescouts/events";
+import { mock } from "@codescouts/test/lib/vitest";
+
+import { LoggerService } from "@/domain/services";
+
+import { TestUseCase } from "./test-use-case";
+
+
+describe("Test use case should", () => {
+    test("call save once time", () => {
+        const loggerService = mock<LoggerService>();
+        const eventDispatcher = mock<IEventDispatcher>();
+        const useCase = new TestUseCase(loggerService, eventDispatcher);
+
+        useCase.execute("Message");
+
+        expect(loggerService.save).toHaveBeenCalledOnce();
+        expect(loggerService.save).toHaveBeenCalledWith(expect.objectContaining({
+            message: "Message"
+        }));
+    });
+
+    test("dispatch the log registered once", () => {
+        const loggerService = mock<LoggerService>();
+        const eventDispatcher = mock<IEventDispatcher>();
+        const useCase = new TestUseCase(loggerService, eventDispatcher);
+
+        useCase.execute("Message");
+
+        expect(eventDispatcher.dispatch).toHaveBeenCalledOnce();
+        expect(eventDispatcher.dispatch).toHaveBeenCalledWith(expect.objectContaining({
+            log: expect.objectContaining({
+                message: "Message"
+            }),
+        }));
+    });
+});
--- apps/starter/vite.config.ts
+++ apps/starter/vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import * as path from 'path'
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+import { defineConfig } from "vite";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  test: {
+    globals: true,
+    environment: "happy-dom",
   },
-})
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+});
