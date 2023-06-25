f6da9168397b3980f54fbf08a610a5909a7bfec9
Damián Pumar <damianpumar@gmail.com>
2023-06-25 19:26:57 +0200

    ✨ Add github repo

--- apps/starter/src/ui/components/header/Header.tsx
+++ apps/starter/src/ui/components/header/Header.tsx
@@ -3,26 +3,36 @@ import styles from "./Header.module.css";
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <img
-        className={styles.logo}
-        src="./favicon.png"
-        width="40px"
-        alt="codescouts-logo"
-      />
-      <span
-        style={{
-          fontWeight: "bold",
-          fontSize: "1.7rem",
-        }}
-      >
-        <span>{"<Code"}</span>
+      <div className={styles.brand}>
+        <img
+          className={styles.logo}
+          src="./favicon.png"
+          width="40px"
+          alt="codescouts-logo"
+        />
         <span
           style={{
-            color: "#ffe102",
+            fontWeight: "bold",
+            fontSize: "1.7rem",
           }}
         >
-          {"Scouts/>"}
+          <span>{"<Code"}</span>
+          <span
+            style={{
+              color: "#ffe102",
+            }}
+          >
+            {"Scouts/>"}
+          </span>
         </span>
+      </div>
+      <span>
+        <a href="https://curtme.org/4hAvORb" title="Github repo">
+          <img
+            src="https://img.shields.io/static/v1?label=codescouts-academy&message=react-clean-architecture&color=blue&logo=github"
+            alt="codescouts-academy - react-clean-architecture"
+          />
+        </a>
       </span>
     </header>
   );
